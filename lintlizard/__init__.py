b5bc2687d3a797177c88c3ddb4046f27be4547fd
Vyacheslav Tverskoy <tsx@tsx.su>
2020-10-23 18:42:22 +0000

    Initial commit

---
+++ lintlizard/__init__.py
@@ -0,0 +1,31 @@
+import subprocess
+
+TOOLS = [
+    ['flake8', '--version'],
+    ['flake8'],
+    ['isort', '--version'],
+    ['isort', '-rc', '-c', '.'],
+    ['mypy', '--version'],
+    ['mypy'],
+    ['black', '--version'],
+    ['black', '--check', '.'],
+]
+
+
+def execute_tools() -> bool:
+    for tool in TOOLS:
+        try:
+            subprocess.run(args=tool, check=True)
+        except subprocess.CalledProcessError:
+            return False
+
+    return True
+
+
+def main() -> None:
+    if not execute_tools():
+        exit(1)
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+setuptools.setup(
+    name="lintlizard",
+    version="0.0.1",
+    author="Vyacheslav Tverskoy",
+    author_email="v@close.com",
+    description="A small example package",
+    url="https://github.com/closeio/lintlizard",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'black==20.8b1',
+        'flake8==3.8.3',
+        'flake8-bugbear==20.1.4',
+        'flake8-docstrings==1.5.0',
+        'flake8-future-import==0.4.6',
+        'flake8-no-u-prefixed-strings==0.2',
+        'flake8-polyfill==1.0.2',
+        'flake8-tidy-imports==4.1.0',
+        'isort==4.3.21',
+        'mccabe==0.6.1',
+        'mypy==0.780',
+        'mypy-extensions==0.4.3',
+        'pathspec==0.8.0',
+        'pep8==1.7.1',
+        'pep8-naming==0.11.1',
+        'pycodestyle==2.6.0',
+        'pydocstyle==5.1.1',
+        'pyflakes==2.2.0',
+        'snowballstemmer==2.0.0',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: POSIX',
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'lintlizard=lintlizard:main',
+        ],
+    },
+)
